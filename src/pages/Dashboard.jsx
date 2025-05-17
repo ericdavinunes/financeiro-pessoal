78f6a0f3c232455bf31c699752b18afe495621d9
Éric Davi Nunes <162338194+ericdavinunes@users.noreply.github.com>
2025-05-16 23:07:13 -0300

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -11,7 +11,7 @@ export default function Dashboard() {
   }, []);
 
   return (
-   <div class="container-fluid">
+   <div class='container-fluid'>
 
       <h1>Dashboard</h1>
       <p>Saldo: R$ {saldo.toFixed(2)}</p>
